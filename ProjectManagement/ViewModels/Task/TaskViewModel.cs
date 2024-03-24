d45fcd5ec9bd62dc0fc84f5363721004f67afc8a
Pezeshkian <Pezeshkian@IT23>
2024-03-24 09:08:41 +0330

    changes

--- ProjectManagement/ViewModels/Task/TaskViewModel.cs
+++ ProjectManagement/ViewModels/Task/TaskViewModel.cs
@@ -6,17 +6,17 @@ namespace ProjectManagement.ViewModels
     public class TaskViewModel
     {
         public int Id { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public string Description { get; set; }
-        public DateTime CreationDate { get; set; }
+        //[Required]
+        public string? Title { get; set; }
+        //[Required]
+        public string? Description { get; set; }
+        public DateTime? CreationDate { get; set; }
         public DateTime? TaskDeadline { get; set; }
         
         public List<int>? TaskCategoryIds { get; set; }
 
         public List<int>? TaskUserIds { get; set; }
-        public int CreatorId { get; set; }
+        public int? CreatorId { get; set; }
         public bool? TaskStatus { get; set; }
 
     }
